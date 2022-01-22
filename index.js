0cbacd140b9fb1ef02a3da6bb1fc11b0976df153
Yashwanth Krishna <72192888+Alone-Y154@users.noreply.github.com>
2022-01-22 16:24:58 +0530

    Added initial files

---
+++ index.js
@@ -0,0 +1,31 @@
+$(document).on('click','.nav', function() {
+
+  $('.nav').removeClass('active');
+  $(this).addClass('active');
+
+});
+
+document.querySelector('.hamburger').addEventListener('click', () => {
+  document.querySelector('.nav-links').classList.toggle('drop');
+});
+
+
+window.addEventListener('scroll', () => {
+  let current = '';
+  // console.log(pageYOffset);
+  document.querySelectorAll('section').forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+
+    if (pageYOffset >= (sectionTop - sectionHeight/3)) {
+      current = section.getAttribute('id');
+    }
+    // console.log(sectionTop);
+  });
+  document.querySelectorAll('.nav-list').forEach((a) => {
+    a.classList.remove('active');
+    if (a.classList.contains(current)) {
+      a.classList.add('active');
+    }
+  });
+});
